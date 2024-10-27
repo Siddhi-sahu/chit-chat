5b1fe173618b40e277f48f3be4326fb367e7120e
Siddhi Sahu <himanisahu739@gmail.com>
2024-10-27 20:27:42 +0530

    added db entry on signin

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,22 +1,24 @@
+import prisma from "@/lib/db";
 import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
+import Google from "next-auth/providers/google";
+// import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
     providers: [
-        CredentialsProvider({
-            name: 'Credentials',
-            credentials: {
-                Email: { label: 'email', type: 'text', placeholder: "email" },
-                password: { label: 'password', type: 'password', placeholder: "password" }
-            },
-            async authorize(credentials: any) {
-                return {
-                    id: "user1"
-                };
-            }
+        // CredentialsProvider({
+        //     name: 'Credentials',
+        //     credentials: {
+        //         Email: { label: 'email', type: 'text', placeholder: "email" },
+        //         password: { label: 'password', type: 'password', placeholder: "password" }
+        //     },
+        //     async authorize(credentials: any) {
+        //         return {
+        //             id: "user1"
+        //         };
+        //     }
 
-        }),
+        // }),
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID ?? "",
             clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
@@ -24,9 +26,25 @@ const handler = NextAuth({
     ],
     secret: process.env.NEXTAUTH_SECRET,
     callbacks: {
-        jwt: ({ token, user }) => {
-            token.userId = token.sub;
-            return token
+        async signIn(params) {
+            if (!params.user.email) {
+                return false
+            }
+
+            try {
+                await prisma.user.create({
+                    data: {
+                        email: params.user.email,
+                        provider: "Google"
+
+                    }
+                })
+
+            } catch (e) {
+                console.log(e)
+            }
+
+            return true;
         }
     }
 });
