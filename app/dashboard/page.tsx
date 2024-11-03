0af804b5cf9ed9e99421dbde805cfae3a2a3d3e8
Siddhi Sahu <himanisahu739@gmail.com>
2024-11-03 21:22:33 +0530

    server init

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function Dashboard() {
+    return <div>
+        hii there from dashboard
+    </div>
+}
--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { Appbar } from "@/components/Appbar";
 export default function Home() {
   return (
     <div>
+      {/* landing page for non-signined users and redirection logic for signined users */}
       <Appbar />
     </div>
   );
