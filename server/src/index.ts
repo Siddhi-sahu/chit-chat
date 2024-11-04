c24efc1e91c5adfb3b5103ba84c5025f693b8cb1
Siddhi Sahu <himanisahu739@gmail.com>
2024-11-04 14:33:05 +0530

    socket server init

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,13 @@
 import http from 'http';
+import SocketService from './services/socket';
 
 async function init() {
     const httpServer = http.createServer();
+    const socketService = new SocketService()
     const PORT = process.env.PORT ? process.env.PORT : 8000;
+
+    //attaching socket io to our http server, after this our socket server will start running
+    socketService.io.attach(httpServer);
     httpServer.listen(PORT, () =>
         console.log(`HTTP Server started at PORT: ${PORT}`)
     )
