903db4a82464303fbccc24674b0e0b9bd44c9dad
Siddhi Sahu <himanisahu739@gmail.com>
2024-11-15 19:33:42 +0530

    add frontend design

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,20 +6,24 @@ import { useState } from "react";
 export default function Dashboard() {
     const { sendMessage, messages } = useSocket();
     const [messsage, setMessage] = useState('');
-    return <div>
+    return <div className="w-full h-screen bg-pink-300 flex justify-center items-start" >
 
-        <div>
-            <input onChange={(e) => {
-                setMessage(e.target.value)
-            }} placeholder="..." className="w-48 h-12 p-2 rounded-lg border-black border-2 border-solid text-black" />
-            <button onClick={(e) => {
-                sendMessage(messsage)
-            }} className="w-12 h-11 p-2 rounded-lg bg-slate-200 text-black">Send</button>
-        </div>
-        <div>
-            <div>
-                {messages.map((e) => <li>{e}</li>)}
+        <div style={{ minHeight: '35rem' }} className="flex flex-col bg-white w-2/6 rounded m-2">
+            <div >
+                <input onChange={(e) => {
+                    setMessage(e.target.value)
+                }} placeholder="..." className="w-3/4 h-12 m-4 p-2 rounded-lg border-black border-2 border-solid text-black" />
+                <button onClick={(e) => {
+                    sendMessage(messsage)
+                }} className="w-14 h-11 p-2 rounded-lg bg-slate-200 text-black">Send</button>
             </div>
+            <div>
+                <div className="m-4">
+                    {messages.map((e) => <li>{e}</li>)}
+                </div>
+            </div>
+
+
         </div>
 
     </div>
--- components/Appbar.tsx
+++ components/Appbar.tsx
@@ -15,14 +15,14 @@ export const Appbar = () => {
     const session = useSession();
 
     return (
-        <div className="flex justify-between">
-            <div>
+        <div className="flex justify-between bg-pink-400">
+            <div className="flex justify-between items-center text-slate-600 text-lg ">
                 Chit-Chat
             </div>
 
             <div>
-                {session.data?.user && <button onClick={() => signOut()}>LogOut</button>}
-                {!session.data?.user && <button onClick={handleSignIn}>Signin</button>}
+                {session.data?.user && <button className="p-2 m-2 bg-pink-200 rounded text-slate-600 text-lg" onClick={() => signOut()}>LogOut</button>}
+                {!session.data?.user && <button className="p-2 m-2 bg-pink-200 rounded text-slate-600 text-lg" onClick={handleSignIn}>Signin</button>}
 
             </div>
         </div>
